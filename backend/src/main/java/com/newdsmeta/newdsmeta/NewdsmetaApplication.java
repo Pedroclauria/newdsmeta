477f1c7da74deacb143ced6bf01b3e027a365477
Pedro Lauria <pedroclauria@gail.com>
2022-09-05 09:51:56 -0300

    Project created

---
+++ backend/src/main/java/com/newdsmeta/newdsmeta/NewdsmetaApplication.java
@@ -0,0 +1,13 @@
+package com.newdsmeta.newdsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewdsmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewdsmetaApplication.class, args);
+	}
+
+}
